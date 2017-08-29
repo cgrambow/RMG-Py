dc8aa90165d97fd7d2c133a6c91b545e3e95576c
cgrambow <cgrambow@mit.edu>
2017-08-29 14:22:24 -0400

    Output RMG object in sensitivity.py and create new script

---
+++ scripts/sensitivity2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script runs stand-alone sensitivity analysis on an RMG job with extra
+functionality.
+"""
+
+import os.path
+import argparse
+
+from rmgpy.tools.sensitivity import runSensitivity
+
+
+################################################################################
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', metavar='INPUT', type=str, nargs=1,
+                        help='RMG input file')
+    parser.add_argument('chemkin', metavar='CHEMKIN', type=str, nargs=1,
+                        help='Chemkin file')
+    parser.add_argument('dictionary', metavar='DICTIONARY', type=str, nargs=1,
+                        help='RMG dictionary file')
+    parser.add_argument('--no-dlim', dest='dlim', action='store_false',
+                        help='Turn off diffusion-limited rates for LiquidReactor')
+    args = parser.parse_args()
+
+    inputFile = os.path.abspath(args.input[0])
+    chemkinFile = os.path.abspath(args.chemkin[0])
+    dictFile = os.path.abspath(args.dictionary[0])
+    dflag = args.dlim
+
+    return inputFile, chemkinFile, dictFile, dflag
+
+
+def main():
+    inputFile, chemkinFile, dictFile, dflag = parse_arguments()
+    rmg = runSensitivity(inputFile, chemkinFile, dictFile, dflag)
+
+
+################################################################################
+
+if __name__ == '__main__':
+    main()
